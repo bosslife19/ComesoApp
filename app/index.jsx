0509c5f04b4cc5a6e0daaa269e375587356c2ea7
bosslife19 <wokodavid001@gmail.com>
2025-01-26 18:11:04 +0000

    new

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
  import { Redirect } from 'expo-router';
  import React from 'react'
+ import registerNNPushToken from 'native-notify';
  export default function index() {
+    registerNNPushToken(26812, 'YbFosVTb3J7wbRaVwDsjuW');
     return <Redirect href={"/(routes)/onboarding"} />;
 }
