3f4ba22b8a05085ae3973a5e4bc6518002dfe79c
bosslife19 <wokodavid001@gmail.com>
2025-01-30 10:46:54 +0000

    made more changes

--- app/(routes)/complain/index.jsx
+++ app/(routes)/complain/index.jsx
@@ -54,134 +54,139 @@ const Complain = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} 
+  style={{ flex: 1 }}>
       <ScrollView>
-        <CustomHeader text="Complain" />
-        <Text
-          style={{
-            marginLeft: "5%",
-            marginTop: "10%",
-            color: "rgba(35, 48, 59, 1)",
-            fontFamily: "Sofia",
-            fontSize: 19,
-            fontWeight: "600",
-          }}
-        >
-          Enter your details
-        </Text>
-        <KeyboardAvoidingView>
-          <View style={{ gap: 10, paddingHorizontal: "5%" }}>
-            <TextInput
-              style={[
-                styles.input,
-                {
-                  backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26",
-                  marginVertical: 10,
-                },
-              ]}
-              keyboardType="default"
-              value={firstName}
-              placeholder="First name"
-              onChangeText={(value) => {
-                setFirstName(value);
-              }}
-              placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
-              onFocus={() => setIsFocuses(true)}
-              onBlur={() => setIsFocuses(false)}
-            />
-            <TextInput
-              style={[
-                styles.input,
-                { backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26" },
-              ]}
-              keyboardType="default"
-              value={lastName}
-              placeholder="Last name"
-              onChangeText={(value) => {
-                setLastName(value);
-              }}
-              placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
-              onFocus={() => setIsFocuses(true)}
-              onBlur={() => setIsFocuses(false)}
-            />
-            <TextInput
-              style={[
-                styles.input,
-                { backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26" },
-              ]}
-              keyboardType="default"
-              placeholder="Email Address"
-              value={email}
-              onChangeText={(value) => {
-                setEmail(value);
-              }}
-              placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
-              onFocus={() => setIsFocuses(true)}
-              onBlur={() => setIsFocuses(false)}
-            />
-          </View>
 
-          <Text
-            style={{
-              marginLeft: "5%",
-              marginTop: "10%",
-              color: "rgba(35, 48, 59, 1)",
-              fontFamily: "Sofia",
-              fontSize: 19,
-              fontWeight: "600",
-            }}
-          >
-            Enter your Complain
-          </Text>
-          <View
-            style={{
-              backgroundColor: "rgba(164, 169, 174, 0.2)",
-              width: "90%",
-              marginHorizontal: "auto",
-              marginTop: 20,
-              borderRadius: 10,
-              paddingVertical: 5,
-              paddingLeft: "3%",
-              height: "25%",
-            }}
-          >
-            <TextInput
-              placeholder="Type your message here..."
-              placeholderTextColor={Platform.OS === "ios" && "#aaa"}
-              style={{ color: Platform.OS == "ios" && "#aaa" }}
-              value={complain}
-              onChangeText={(val) => setComplain(val)}
-            />
-          </View>
-          <TouchableOpacity
-            style={{
-              width: "80%",
-              backgroundColor: "rgba(10, 46, 226, 1)",
-              height: 60,
-              alignSelf: "center",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 10,
-              marginTop: "10%",
-              marginBottom: 20,
-            }}
-            onPress={handleComplain}
-          >
-            {loading ? (
-              <ActivityIndicator color="white" size="small" />
-            ) : (
-              <Text
-                style={{
-                  color: "white",
-                  fontSize: 18,
-                  marginLeft: 10,
-                }}
-              >
-                Save
-              </Text>
-            )}
-          </TouchableOpacity>
-        </KeyboardAvoidingView>
-      </ScrollView>
+<CustomHeader text="Complain" />
+<Text
+  style={{
+    marginLeft: "5%",
+    marginTop: "10%",
+    color: "rgba(35, 48, 59, 1)",
+    fontFamily: "Sofia",
+    fontSize: 19,
+    fontWeight: "600",
+  }}
+>
+  Enter your details
+</Text>
+
+  <View style={{ gap: 10, paddingHorizontal: "5%" }}>
+    <TextInput
+      style={[
+        styles.input,
+        {
+          backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26",
+          marginVertical: 10,
+        },
+      ]}
+      keyboardType="default"
+      value={firstName}
+      placeholder="First name"
+      onChangeText={(value) => {
+        setFirstName(value);
+      }}
+      placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
+      onFocus={() => setIsFocuses(true)}
+      onBlur={() => setIsFocuses(false)}
+    />
+    <TextInput
+      style={[
+        styles.input,
+        { backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26" },
+      ]}
+      keyboardType="default"
+      value={lastName}
+      placeholder="Last name"
+      onChangeText={(value) => {
+        setLastName(value);
+      }}
+      placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
+      onFocus={() => setIsFocuses(true)}
+      onBlur={() => setIsFocuses(false)}
+    />
+    <TextInput
+      style={[
+        styles.input,
+        { backgroundColor: isFocuses ? "#FFFFFF" : "#A4A9AE26" },
+      ]}
+      keyboardType="default"
+      placeholder="Email Address"
+      value={email}
+      onChangeText={(value) => {
+        setEmail(value);
+      }}
+      placeholderTextColor={Platform.OS === "ios" ? "#aaa" : "#8E949A"}
+      onFocus={() => setIsFocuses(true)}
+      onBlur={() => setIsFocuses(false)}
+    />
+  </View>
+
+  <Text
+    style={{
+      marginLeft: "5%",
+      marginTop: "10%",
+      color: "rgba(35, 48, 59, 1)",
+      fontFamily: "Sofia",
+      fontSize: 19,
+      fontWeight: "600",
+    }}
+  >
+    Enter your Complain
+  </Text>
+  <View
+    style={{
+      backgroundColor: "rgba(164, 169, 174, 0.2)",
+      width: "90%",
+      marginHorizontal: "auto",
+      marginTop: 20,
+      borderRadius: 10,
+      paddingVertical: 5,
+      paddingLeft: "3%",
+      height: "25%",
+    }}
+  >
+    <TextInput
+      placeholder="Type your message here..."
+      placeholderTextColor={Platform.OS === "ios" && "#aaa"}
+      style={{ color: Platform.OS == "ios" && "#aaa" }}
+      value={complain}
+      onChangeText={(val) => setComplain(val)}
+    />
+  </View>
+  <TouchableOpacity
+    style={{
+      width: "80%",
+      backgroundColor: "rgba(10, 46, 226, 1)",
+      height: 60,
+      alignSelf: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      marginTop: "10%",
+      marginBottom: 20,
+    }}
+    onPress={handleComplain}
+  >
+    {loading ? (
+      <ActivityIndicator color="white" size="small" />
+    ) : (
+      <Text
+        style={{
+          color: "white",
+          fontSize: 18,
+          marginLeft: 10,
+        }}
+      >
+        Save
+      </Text>
+    )}
+  </TouchableOpacity>
+
+</ScrollView>
+      </KeyboardAvoidingView>
+      
     </SafeAreaView>
   );
 };
