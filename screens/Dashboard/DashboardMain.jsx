9d0887f82f67d5c23f32fbb21efe2ad2536a9929
bosslife19 <wokodavid001@gmail.com>
2025-07-08 19:40:55 +0100

    fixed up google map api

--- screens/Dashboard/DashboardMain.jsx
+++ screens/Dashboard/DashboardMain.jsx
@@ -10,9 +10,20 @@ const DashboardMain = () => {
     const sendPushToken = async ()=>{
       const token = await AsyncStorage.getItem('pushToken');
       if(!token){
+       
+         registerForPushNotificationsAsync().then(
+                (token) => {
+                  axiosClient.post('/user-push-token', {token}).then(data=>AsyncStorage.removeItem('pushToke'));
+    
+                },
+                (error) => {
+                  console.log(error)
+                }
+              );
         return;
       }
      const res =  await axiosClient.post('/user-push-token', {token});
+
      if(res.data.status){
       await AsyncStorage.removeItem('pushToken');
      }
